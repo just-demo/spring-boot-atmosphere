dceac2225dff2cac6df311db54b50108ca86d69d
Anatolii Kryvda <anatolii.kryvda@automationanywhere.com>
2019-02-28 13:29:10 +0200

    Initial commit

---
+++ src/main/java/self/ed/springbootatmosphere/SpringBootAtmosphereApplication.java
@@ -0,0 +1,13 @@
+package self.ed.springbootatmosphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAtmosphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAtmosphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/self/ed/springbootatmosphere/SpringBootAtmosphereApplicationTests.java
@@ -0,0 +1,16 @@
+package self.ed.springbootatmosphere;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootAtmosphereApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
